8f80b7e5f21e503ef35947b9c4e0059f8960f270
Ashish Kumar <ashish.k@mindfiresolutions.com>
2017-10-26 18:19:05 +0530

    Added i18n for burmese.

--- src/main/java/com/todoapp/config/AppConfig.java
+++ src/main/java/com/todoapp/config/AppConfig.java
@@ -1,9 +1,16 @@
 package com.todoapp.config;
 
+
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.todoapp.interceptor.RequestInterceptor;
 
@@ -16,8 +23,23 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 	@Autowired
 	RequestInterceptor requestInterceptor;
 	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.US);
+	    return slr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    return lci;
+	}
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(requestInterceptor);
+		registry.addInterceptor(this.localeChangeInterceptor());
 	}
 }
